5fcda8938c1d368bae1419873f6216fc631b9064
Automoroboto <roberto@studiodespirito.co.uk>
2023-04-27 13:32:46 +0100

    minor tweak to spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,12 @@ import './index.css';
 const Home = () => {
   return (
     <div className='page'>
+      <div className="sidebar">
       {/* <nav>
         <ul>
           <li><a href="/">Home</a></li>
         </ul>
         </nav> */}
-      <div className="sidebar">
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-        </ul>
-        </nav>
       <h1>StudySnap</h1>
       <p className="subheading">Introducing StudySnap – the app that makes studying easier and more collaborative than ever before.</p>
       <p>  With StudySnap, students can post and upvote their favorite essential slides from lectures or presentations, allowing everyone to benefit from the best take-home slides.
